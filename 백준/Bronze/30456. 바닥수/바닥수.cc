14a146719e83c0273599e4fe6dd0a49e4d721721
MongsangGa <88891106+MongsangGa@users.noreply.github.com>
2023-11-12 14:48:03 +0900

    [Bronze I] Title: 바닥수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30456. 바닥수/바닥수.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, p;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cin >> n >> p;
+    for (int i = 0; i < p - 1; i++) cout << "1";
+    cout << n;
+    return 0;
+}
