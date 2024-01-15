21e15cdf6947e4fa8a3c53f01a52076a359b17ce
MongsangGa <88891106+MongsangGa@users.noreply.github.com>
2024-01-15 14:24:20 +0900

    [Gold IV] Title: 최단경로, Time: 100 ms, Memory: 9184 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.cc
+++ 백준/Gold/1753. 최단경로/최단경로.cc
@@ -1,34 +1,38 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+
 using namespace std;
+
+#define SIZE 20'002
 #define INF 1e9+1
-int n,m,k,v,a,b,d[20005];
-vector<pair<int, int>> node[20005];
-priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
-int main(){
-    scanf("%d%d%d",&n,&m,&k);
-    fill(d,d+n+1,INF);
-    for(int i=0;i<m;i++){
-        scanf("%d%d%d",&a,&b,&v);
-        node[a].push_back({v,b}); // cost, end
+int V, E, start, d[SIZE];
+vector<pair<int, int>> adj[SIZE]; // 비용, 간선
+priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin >> V >> E >> start;
+    for (int i = 0; i < E; i++) {
+        int u, v, w;
+        cin >> u >> v >> w;
+        adj[u].push_back({w, v});
     }
-    d[k]=0;
-    pq.push({d[k],k});
-    while(!pq.empty()){
-        auto cur=pq.top(); pq.pop();
-        int idx,cost; tie(cost,idx)=cur;
-        if(d[idx]!=cost) continue;// 이전에 최소값이 들어간 경우
-        for(auto nxt:node[idx]){
-            int nidx,ncost; tie(ncost,nidx)=nxt;
-            if(d[nidx]>cost+ncost){
-                d[nidx]=cost+ncost;
-                pq.push({d[nidx], nidx});
-            }
+    fill(d, d + SIZE, INF);
+    d[start] = 0;
+    pq.push({d[start], start});
+    while (!pq.empty()) {
+        auto cur = pq.top();
+        pq.pop();
+        if (d[cur.second] != cur.first) continue;
+        for (auto nxt: adj[cur.second]) {
+            if (d[nxt.second] <= d[cur.second] + nxt.first) continue;
+            d[nxt.second] = d[cur.second] + nxt.first;
+            pq.push({d[nxt.second], nxt.second});
         }
     }
-    for(int i=1;i<=n;i++){
-        if(d[i]==INF) printf("INF\n");
-        else printf("%d\n",d[i]);
+    for (int i = 1; i <= V; i++) {
+        if (d[i] == INF) cout << "INF\n";
+        else cout << d[i] << '\n';
     }
     return 0;
-
 }
