cd98cbc0e95de1260b1793cba4689fad87d66756
MongsangGa <88891106+MongsangGa@users.noreply.github.com>
2024-01-26 15:20:15 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 8 ms, Memory: 2056 KB -BaekjoonHub

--- 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.cc
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.cc
@@ -7,7 +7,7 @@ int dp[21][21][21], a, b, c;
 int f(int a, int b, int c) {
     if (a <= 0 || b <= 0 || c <= 0) return 1;
     if (a > 20 || b > 20 || c > 20) return f(20, 20, 20);
-    int &cur = dp[a][b][c];
+    int cur = dp[a][b][c];
     if (cur != -1) return cur;
     if (a < b && b < c) return dp[a][b][c] = f(a, b, c - 1) + f(a, b - 1, c - 1) - f(a, b - 1, c);
     else return dp[a][b][c] = f(a - 1, b, c) + f(a - 1, b - 1, c) + f(a - 1, b, c - 1) - f(a - 1, b - 1, c - 1);
