83c5ff0b3d88a8a59ebce8a1489926a0da2c4b7e
MongsangGa <88891106+MongsangGa@users.noreply.github.com>
2024-02-01 16:55:05 +0900

    [Silver II] Title: 섬의 개수, Time: 0 ms, Memory: 2180 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.cc
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.cc
@@ -1,43 +1,46 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
-typedef pair<int, int> pii;
-#define MAX 51
 
-int n, m, num, board[MAX][MAX], dx[] = {-1, 0, 1, 0, 1, 1, -1, -1}, dy[] = {0, -1, 0, 1, 1, -1, 1, -1};
-
-void solve(int x, int y) {
-    for (int i = 0; i < 8; i++) {
-        int nx = x + dx[i], ny = y + dy[i];
-        if (nx < 0 || ny < 0 || nx >= n || ny >= m || !board[nx][ny]) continue;
-        board[nx][ny] = 0;
-        solve(nx, ny);
-    }
-}
+#define fastio cin.tie(0), ios_base::sync_with_stdio(false);
+#define SIZE 52
+int n, m, board[SIZE][SIZE], vis[SIZE][SIZE], dx[] = {-1, 0, 1, 0, -1, -1, 1, 1}, dy[] = {0, -1, 0, 1, -1, 1, -1, 1};
+queue<pair<int, int>> q;
 
 int main() {
+    fastio;
     while (1) {
-        num = 0;
-        queue<pii> q;
         cin >> m >> n;
         if (!n && !m) break;
+        fill(board[0], board[SIZE - 1], 0);
+        fill(vis[0], vis[SIZE - 1], 0);
+        vector<pair<int, int>> start;
+        int res = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 cin >> board[i][j];
                 if (board[i][j])
-                    q.push({i, j});
+                    start.push_back({i, j});
             }
         }
-        while (!q.empty()) {
-            int x, y;
-            tie(x, y) = q.front();
-            q.pop();
-            if (!board[x][y]) continue;
-            num++;
-            board[x][y] = 0;
-            solve(x, y);
+        for (auto [sX, sY]: start) {
+            if (vis[sX][sY]) continue;
+            res++;
+            vis[sX][sY] = 1;
+            q.push({sX, sY});
+            while (!q.empty()) {
+                auto [x, y] = q.front();
+                q.pop();
+                for (int i = 0; i < 8; i++) {
+                    int nx = x + dx[i], ny = y + dy[i];
+                    if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
+                    if (vis[nx][ny] || !board[nx][ny]) continue;
+                    vis[nx][ny] = 1;
+                    q.push({nx, ny});
+                }
+            }
         }
-        cout << num << '\n';
+        cout << res << '\n';
     }
     return 0;
 }
