3ab9d0c5c12ac34fb71a6cdb096f28d9a6a9b5f8
MongsangGa <88891106+MongsangGa@users.noreply.github.com>
2024-02-06 14:36:55 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 3, Time: 188 ms, Memory: 8292 KB -BaekjoonHub

---
+++ 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fastio cin.tie(0), ios_base::sync_with_stdio(false);
+
+int n, a;
+vector<int> v;
+
+int main() {
+    fastio;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> a;
+        if (v.empty() || v.back() < a) v.push_back(a);
+        else {
+            int idx = lower_bound(v.begin(), v.end(), a) - v.begin();
+            v[idx] = a;
+        }
+    }
+    cout << v.size();
+    return 0;
+}
