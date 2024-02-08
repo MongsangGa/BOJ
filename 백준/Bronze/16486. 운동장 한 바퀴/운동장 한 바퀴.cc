e5d5d8589442515d32b60bb208e3c78167aa505d
MongsangGa <88891106+MongsangGa@users.noreply.github.com>
2024-02-08 18:00:41 +0900

    [Bronze IV] Title: 운동장 한 바퀴, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16486. 운동장 한 바퀴/운동장 한 바퀴.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fastio cin.tie(0), ios_base::sync_with_stdio(false);
+
+double d1, d2;
+
+int main() {
+    fastio
+    cin >> d1 >> d2;
+    printf("%.6f\n", d1 * 2 + 2 * 3.141592 * d2);
+    return 0;
+}
