399ae78a5e6ba2126a2b0e7239b460505ed91bcf
MongsangGa <88891106+MongsangGa@users.noreply.github.com>
2024-02-18 02:33:32 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.cc
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.cc
@@ -3,32 +3,21 @@
 using namespace std;
 #define fastio cin.tie(0), ios_base::sync_with_stdio(0)
 #define INF 1'000'000'002
-int n, res = INF, idx[11];
-vector<pair<int, int>> v;
 
-int cal(int len) {
-    int res1 = 1, res2 = 0;
-    for (int i = 0; i < len; i++)
-        if (idx[i])
-            res1 *= v[i].first, res2 += v[i].second;
-    return min(abs(res1 - res2), abs(res2 - res1));
-}
-
-void solve(int len, int cnt) {
-    if (len > n) return;
-    if (cnt > 0) res = min(res, cal(len));
-    idx[len] = 1;
-    solve(len + 1, cnt + 1);
-    idx[len] = 0;
-    solve(len + 1, cnt);
-}
+int n, res = INF;
 
 int main() {
     fastio;
     cin >> n;
-    v.resize(n);
+    vector<pair<int, int>> v(n);
     for (int i = 0; i < n; i++) cin >> v[i].first >> v[i].second;
-    solve(0, 0);
+    for (int i = 1; i < (1 << n); i++) {
+        int res1 = 1, res2 = 0;
+        for (int j = 0; j < n; j++)
+            if (i & (1 << j))
+                res1 *= v[j].first, res2 += v[j].second;
+        res = min(res, abs(res1 - res2));
+    }
     cout << res;
     return 0;
 }
