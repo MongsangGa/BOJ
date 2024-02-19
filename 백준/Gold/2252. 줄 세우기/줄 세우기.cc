d479a1fbaeb5b883d7676eac12b6656616af4d5b
MongsangGa <88891106+MongsangGa@users.noreply.github.com>
2024-02-19 21:31:20 +0900

    [Gold III] Title: 줄 세우기, Time: 28 ms, Memory: 3956 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.cc
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.cc
@@ -1,29 +1,31 @@
 #include <bits/stdc++.h>
+
 using namespace std;
-int n,m,ind[32001];
+#define fastio cin.tie(0), ios_base::sync_with_stdio(0)
+#define SIZE 32'002
+int n, m, ind[SIZE];
+vector<int> adj[SIZE];
 queue<int> q;
-vector<int> v[32001];
-int main(){
-    cin.tie(0);
-    ios_base::sync_with_stdio(0);
-    cin>>n>>m;
-    while(m--){
-        int a,b;
-        cin>>a>>b;
-        v[a].push_back(b);
+
+int main() {
+    fastio;
+    cin >> n >> m;
+    while (m--) {
+        int a, b;
+        cin >> a >> b;
+        adj[a].push_back(b);
         ind[b]++;
     }
-    for(int i=1;i<=n;i++){
-        if(!ind[i])
+    for (int i = 1; i <= n; i++)
+        if (!ind[i])
             q.push(i);
-    }
-    while(!q.empty()){
-        int cur=q.front(); q.pop();
-        cout<<cur<<' ';
-        for(int nxt:v[cur]){
+    while (!q.empty()) {
+        int cur = q.front();
+        q.pop();
+        cout << cur << ' ';
+        for (auto nxt: adj[cur]) {
             ind[nxt]--;
-            if(!ind[nxt])
-                q.push(nxt);
+            if (!ind[nxt]) q.push(nxt);
         }
     }
     return 0;
