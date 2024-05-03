e79b8b0322401f94054740435cb5a087462e1e1c
MongsangGa <88891106+MongsangGa@users.noreply.github.com>
2024-05-04 03:13:43 +0900

    [Platinum IV] Title: 걷는 산타클로스, Time: 40 ms, Memory: 5156 KB -BaekjoonHub

--- 백준/Platinum/5552. 걷는 산타클로스/걷는 산타클로스.cc
+++ 백준/Platinum/5552. 걷는 산타클로스/걷는 산타클로스.cc
@@ -5,7 +5,8 @@ using namespace std;
 #define fastio cin.tie(NULL), ios_base::sync_with_stdio(false)
 #define int long long
 int w, h, n, rx, ry, res = LLONG_MAX;
-vector<pair<int, int>> p, px, py;
+vector<int> x, y;
+vector<pair<int, int>> p;
 
 void calc(int tx, int ty) {
     int ans = 0, MAX = 0;
@@ -23,20 +24,19 @@ void calc(int tx, int ty) {
 int32_t main() {
     fastio;
     cin >> w >> h >> n;
-    p.resize(n);
-    for (int i = 0; i < n; i++)
-        cin >> p[i].first >> p[i].second;
-    px = p, py = p;
-    sort(px.begin(), px.end());
-    sort(py.begin(), py.end(), [](pair<int, int> a, pair<int, int> b) {
-        return a.second < b.second;
-    });
-    if (n & 1) calc(px[n / 2].first, py[n / 2].second);
+    x.resize(n), y.resize(n), p.resize(n);
+    for (int i = 0; i < n; i++) {
+        int a, b;
+        cin >> a >> b;
+        x[i] = a, y[i] = b, p[i] = {a, b};
+    }
+    sort(x.begin(), x.end()), sort(y.begin(), y.end());
+    if (n & 1) calc(x[n / 2], y[n / 2]);
     else {
-        calc(px[n / 2 - 1].first, py[n / 2 - 1].second);
-        calc(px[n / 2 - 1].first, py[n / 2].second);
-        calc(px[n / 2].first, py[n / 2 - 1].second);
-        calc(px[n / 2].first, py[n / 2].second);
+        calc(x[n / 2 - 1], y[n / 2 - 1]);
+        calc(x[n / 2 - 1], y[n / 2]);
+        calc(x[n / 2], y[n / 2 - 1]);
+        calc(x[n / 2], y[n / 2]);
     }
     cout << res << '\n' << rx << ' ' << ry;
     return 0;
