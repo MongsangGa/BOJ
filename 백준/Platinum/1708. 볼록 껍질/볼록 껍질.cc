a237c2f136aa8cf7025aca97acc00748227c6e2c
MongsangGa <88891106+MongsangGa@users.noreply.github.com>
2024-05-23 20:27:31 +0900

    [Platinum V] Title: 볼록 껍질, Time: 36 ms, Memory: 2808 KB -BaekjoonHub

---
+++ 백준/Platinum/1708. 볼록 껍질/볼록 껍질.cc
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fastio cin.tie(NULL), ios_base::sync_with_stdio(false)
+#define pii pair<int,int>
+#define ll long long
+#define x first
+#define y second
+
+int n;
+vector<pii > v, res;
+
+int CCW(pii a, pii b, pii c) {
+    //ll cw = (a.x * b.y + b.x * c.y + c.x * a.y) - (b.x * a.y + c.x * b.y + a.x * c.y);
+    ll cw = 1LL * (b.x - a.x) * (c.y - b.y) - 1LL * (b.y - a.y) * (c.x - b.x);
+    if (!cw) return 0;
+    if (cw < 0) return -1;
+    if (cw > 0) return 1;
+}
+
+bool compare(pii &a, pii &b) {
+    int cw = CCW(v[0], a, b);
+    if (cw) return cw == 1;
+    return (a.y < b.y || (a.y == b.y && a.x < b.x));
+}
+
+int main() {
+    fastio;
+    cin >> n;
+    v.resize(n);
+    for (int i = 0; i < n; i++) {
+        cin >> v[i].x >> v[i].y;
+        if (v[0].y > v[i].y || (v[0].y == v[i].y && v[0].x > v[i].x))
+            swap(v[0], v[i]);
+    }
+    sort(v.begin() + 1, v.end(), compare);
+    res.push_back(v[0]), res.push_back(v[1]);
+    for (int i = 2; i < n; i++) {
+        while (res.size() >= 2 && CCW(res[res.size() - 2], res.back(), v[i]) != 1) res.pop_back();
+        res.push_back(v[i]);
+    }
+    cout << res.size();
+    return 0;
+}
