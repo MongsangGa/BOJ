5f9dc0fbbc6a5ab560804627e83e9733cf7bf084
MongsangGa <88891106+MongsangGa@users.noreply.github.com>
2024-07-18 20:08:44 +0900

    [Silver V] Title: 돌 게임 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9656. 돌 게임 2/돌 게임 2.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fastio cin.tie(0), ios_base::sync_with_stdio(0)
+
+int n;
+
+int main() {
+    fastio;
+    cin >> n;
+    if (n % 2) cout << "CY";
+    else cout << "SK";
+    return 0;
+}
