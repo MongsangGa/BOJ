81ca9b83dc2aa983635b2ddbc499b8868f56cedc
MongsangGa <88891106+MongsangGa@users.noreply.github.com>
2024-07-29 17:03:09 +0900

    [Silver II] Title: 이항 계수 2, Time: 4 ms, Memory: 5944 KB -BaekjoonHub

--- 백준/Silver/11051. 이항 계수 2/이항 계수 2.cc
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.cc
@@ -3,19 +3,18 @@
 using namespace std;
 
 #define fastio cin.tie(0), ios_base::sync_with_stdio(0)
-#define SIZE 1002
 #define INF 10'007
 
-int n, k, dp[SIZE][SIZE];
+int n, k, dp[1002][1002];
 
 int main() {
     fastio;
     cin >> n >> k;
-    for (int i = 1; i < SIZE; i++)
+    for (int i = 1; i <= n; i++)
         for (int j = 0; j <= i; j++) {
             if (!j || i == j) dp[i][j] = 1;
             else dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j]) % INF;
         }
-    cout << dp[n][k];
+    cout << dp[n][k] % INF;
     return 0;
 }
