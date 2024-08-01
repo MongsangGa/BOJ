afacc379ba5be88b980760ea7a74ab9cae2d1c51
MongsangGa <88891106+MongsangGa@users.noreply.github.com>
2024-08-02 01:48:38 +0900

    [Silver II] Title: 랜선 자르기, Time: 4 ms, Memory: 2184 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
@@ -1,31 +1,34 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
+
 using namespace std;
-long long int K,N,a,res=0;
-vector<long long int> v;
-int main(){
-    cin.tie(NULL);
-    cout.tie(NULL);
-    ios_base::sync_with_stdio(false);
-    cin>>K>>N;
-    for(int i=0;i<K;i++) {
-            cin>>a;
-            v.push_back(a);
+
+#define fastio cin.tie(0), ios_base::sync_with_stdio(0)
+#define ll long long
+int k, n;
+ll res;
+vector<int> v;
+
+int main() {
+    fastio;
+    cin >> k >> n;
+    v.resize(k);
+    for (int i = 0; i < k; i++) cin >> v[i];
+    sort(v.begin(), v.end());
+    ll s = 1, e = v[k - 1], mid;
+    while (s <= e) {
+        mid = (s + e) / 2;
+        int cnt = 0;
+        for (auto i: v) cnt += i / mid;
+        if (cnt >= n) res = max(res, mid), s = mid + 1;
+        else e = mid - 1;
     }
-    sort(v.begin(),v.end());
-    long long int left=1,right=v[K-1],mid;
-    while(left<=right){
-        int cnt=0;
-        mid=(left+right)/2;
-        for(int i=0;i<v.size();i++)
-            cnt+=v[i]/mid;
-        if(cnt>=N){
-             res=max(res,mid);
-             left=mid+1;
-        }
-        else right=mid-1;
-    }
-    cout<<res;
+    cout << res;
     return 0;
 }
+
+/*
+ * 단순 구현의 시간복잡도를 대략 예상해보면 O(N * K * 최대 랜선크기(2^31)) 쯤이다
+ * 반드시 개선해야 한다는 걸 알 수 있는데 N 개 이상 만들 수 있는 랜선 값을
+ * 이분 탐색을 활용하면 O(NlgN) 에 해결할 수 있다
+ * 위와 같은 이분탐색 결정 문제를 Parametric Search 라고 하니 더 찾아보셔도 좋습니다
+ */
