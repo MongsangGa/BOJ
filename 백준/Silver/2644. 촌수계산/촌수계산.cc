4b5dcf99980e35fce2c092605990a1d263082b24
MongsangGa <88891106+MongsangGa@users.noreply.github.com>
2024-08-21 15:57:38 +0900

    [Silver II] Title: 촌수계산, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.cc
+++ 백준/Silver/2644. 촌수계산/촌수계산.cc
@@ -2,37 +2,34 @@
 
 using namespace std;
 
-#define fastio cin.tie(0), ios_base::sync_with_stdio(false);
-#define X first
-#define Y second
-#define SIZE 101
+#define fastio cin.tie(0), ios_base::sync_with_stdio(0)
+#define SIZE 102
 
-int n, m, x, y, vis[SIZE];
+int n, m, tx, ty, vis[SIZE];
 vector<int> adj[SIZE];
-queue<int> q;
+
+void DFS(int now) {
+    for (auto nxt: adj[now]) {
+        if (vis[nxt] != -1) continue;
+        vis[nxt] = vis[now] + 1;
+        DFS(nxt);
+    }
+    return;
+}
 
 int main() {
     fastio;
-    cin >> n >> x >> y >> m;
+    cin >> n >> tx >> ty >> m;
+    fill(vis, vis + SIZE, -1);
     while (m--) {
-        int a, b;
-        cin >> a >> b;
-        adj[a].push_back(b);
-        adj[b].push_back(a);
+        int x, y;
+        cin >> x >> y;
+        adj[x].push_back(y);
+        adj[y].push_back(x);
     }
-    vis[x] = 1;
-    q.push(x);
-    while (!q.empty()) {
-        auto now = q.front();
-        q.pop();
-        for (auto nxt: adj[now]) {
-            if (vis[nxt]) continue;
-            vis[nxt] = vis[now] + 1;
-            q.push(nxt);
-        }
-    }
-    if (!vis[y]) cout << -1;
-    else cout << vis[y] - 1;
+    vis[tx] = 0;
+    DFS(tx);
+    cout << vis[ty];
     return 0;
 }
 
