e3c4f85236d19fb01054ef9d09f7b30004aafa15
MongsangGa <88891106+MongsangGa@users.noreply.github.com>
2024-08-27 16:42:16 +0900

    [Gold III] Title: LCA, Time: 28 ms, Memory: 11344 KB -BaekjoonHub

--- 백준/Gold/11437. LCA/LCA.cc
+++ 백준/Gold/11437. LCA/LCA.cc
@@ -3,22 +3,39 @@
 using namespace std;
 
 #define fastio ios_base::sync_with_stdio(0), cin.tie(0)
-const int SIZE = 100'002;
-int n, m, a, b, dep[SIZE] = {-1}, arr[SIZE][20];
-int max_level = floor(log2(SIZE));
+const int SIZE = 50'005;
+const int max_depth = floor(log2(SIZE));
+
+int n, m, a, b, depth[SIZE], parent[SIZE][21];
+bool vis[SIZE];
 vector<int> adj[SIZE];
 
-void init(int now, int parent) {
-    dep[now] = dep[parent] + 1;
-    arr[now][0] = parent;
-    for (int i = 1; i <= max_level; i++) {
-        int tmp = arr[now][i - 1];
-        arr[now][i] = arr[tmp][i - 1];
-    }
+void DFS(int now, int d) {
+    vis[now] = true;
+    depth[now] = d;
     for (auto nxt: adj[now]) {
-        if (nxt == parent) continue;
-        init(nxt, now);
+        if (vis[nxt]) continue;
+        parent[nxt][0] = now;
+        DFS(nxt, d + 1);
     }
+}
+
+void f() {
+    for (int i = 1; i <= max_depth; i++)
+        for (int j = 1; j <= n; j++)
+            parent[j][i] = parent[parent[j][i - 1]][i - 1];
+}
+
+int LCA(int x, int y) {
+    if (depth[x] > depth[y]) swap(x, y);
+    for (int i = max_depth; i >= 0; i--)
+        if (depth[y] - depth[x] >= (1 << i))
+            y = parent[y][i];
+    if (x == y) return x;
+    for (int i = max_depth; i >= 0; i--)
+        if (parent[x][i] != parent[y][i])
+            x = parent[x][i], y = parent[y][i];
+    return parent[x][0];
 }
 
 int main() {
@@ -29,25 +46,12 @@ int main() {
         adj[a].push_back(b);
         adj[b].push_back(a);
     }
-    init(1, 0);
+    DFS(1, 0);
+    f();
     cin >> m;
     while (m--) {
         cin >> a >> b;
-        if (dep[a] != dep[b]) {
-            if (dep[a] > dep[b]) swap(a, b);
-            for (int i = max_level; i >= 0; i--)
-                if (dep[a] <= dep[arr[b][i]])
-                    b = arr[b][i];
-        }
-        int res = a;
-        if (a != b) {
-            for (int i = max_level; i >= 0; i--) {
-                if (arr[a][i] != arr[b][i])
-                    a = arr[a][i], b = arr[b][i];
-                res = arr[a][i];
-            }
-        }
-        cout << res << '\n';
+        cout << LCA(a, b) << '\n';
     }
     return 0;
 }
