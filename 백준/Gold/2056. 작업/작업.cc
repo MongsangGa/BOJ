85b7d1eb14058d8dd4e6df23b3138ee2688ba5b3
MongsangGa <88891106+MongsangGa@users.noreply.github.com>
2024-09-05 16:38:59 +0900

    [Gold IV] Title: 작업, Time: 56 ms, Memory: 5940 KB -BaekjoonHub

--- 백준/Gold/2056. 작업/작업.cc
+++ 백준/Gold/2056. 작업/작업.cc
@@ -3,41 +3,38 @@
 using namespace std;
 #define fastio cin.tie(0), ios_base::sync_with_stdio(0)
 #define SIZE 10'002
-#define INF 1e9
 
+int n, ind[SIZE], cost[SIZE], ans[SIZE], res;
 vector<int> adj[SIZE];
-int n, m, a, ind[SIZE], cost[SIZE], res[SIZE], ans;
 queue<int> q;
 
 int main() {
     fastio;
     cin >> n;
-    for (int i = 1; i <= n; i++) {
-        cin >> cost[i] >> m;
-        while (m--) {
-            cin >> a;
-            ind[i]++;
-            adj[a].push_back(i);
+    int len, y;
+    for (int x = 1; x <= n; x++) {
+        cin >> cost[x] >> len;
+        while (len--) {
+            cin >> y;
+            adj[y].push_back(x);
+            ++ind[x];
         }
     }
-    fill(res, res + SIZE, INF);
-    for (int i = 1; i <= n; i++) {
+    for (int i = 1; i <= n; i++)
         if (!ind[i])
-            q.push(i);
-        res[i] = cost[i];
-    }
+            q.push(i), ans[i] = cost[i];
     while (!q.empty()) {
-        int cur = q.front();
+        auto now = q.front();
         q.pop();
-        for (auto nxt: adj[cur]) {
-            ind[nxt]--;
-            res[nxt] = max(res[nxt], res[cur] + cost[nxt]);
+        for (auto nxt: adj[now]) {
+            --ind[nxt];
+            ans[nxt] = max(ans[nxt], ans[now] + cost[nxt]);
             if (!ind[nxt])
                 q.push(nxt);
         }
     }
     for (int i = 1; i <= n; i++)
-        ans = max(ans, res[i]);
-    cout << ans;
+        res = max(res, ans[i]);
+    cout << res;
     return 0;
 }
