b2eea2816658d69681b407fded4ac087dccbd29f
MongsangGa <88891106+MongsangGa@users.noreply.github.com>
2024-09-05 11:21:10 +0900

    [Gold III] Title: 음악프로그램, Time: 0 ms, Memory: 2188 KB -BaekjoonHub

--- 백준/Gold/2623. 음악프로그램/음악프로그램.cc
+++ 백준/Gold/2623. 음악프로그램/음악프로그램.cc
@@ -1,10 +1,12 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 
 using namespace std;
-#define fastio cin.tie(0), ios_base::sync_with_stdio(0)
-#define SIZE 1'002
-vector<int> adj[SIZE], res;
+
+#define fastio ios_base::sync_with_stdio(0), cin.tie(0)
+#define SIZE 1'003
+
 int n, m, arr[SIZE], ind[SIZE];
+vector<int> adj[SIZE], res;
 queue<int> q;
 
 int main() {
@@ -16,23 +18,20 @@ int main() {
         for (int i = 0; i < len; i++) cin >> arr[i];
         for (int i = 0; i < len - 1; i++) {
             adj[arr[i]].push_back(arr[i + 1]);
-            ind[arr[i + 1]]++;
+            ++ind[arr[i + 1]];
         }
     }
-    for (int i = 1; i <= n; i++)
-        if (!ind[i])
-            q.push(i);
+    for (int i = 1; i <= n; i++) if (!ind[i]) q.push(i);
     while (!q.empty()) {
-        int cur = q.front();
+        int now = q.front();
         q.pop();
-        res.push_back(cur);
-        for (auto nxt: adj[cur]) {
-            ind[nxt]--;
-            if (!ind[nxt])
-                q.push(nxt);
+        res.push_back(now);
+        for (int nxt: adj[now]) {
+            --ind[nxt];
+            if (!ind[nxt]) q.push(nxt);
         }
     }
-    if (res.size() != n) cout << "0";
-    else for (auto i: res) cout << i << ' ';
+    if (res.size() != n) cout << 0;
+    else for (auto r: res) cout << r << '\n';
     return 0;
 }
