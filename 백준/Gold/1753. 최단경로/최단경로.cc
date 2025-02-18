2391717f1fdcc1c3bd1ca8521b837147ac9fc074
MongsangGa <88891106+MongsangGa@users.noreply.github.com>
2025-02-18 14:09:02 +0900

    [Gold IV] Title: 최단경로, Time: 92 ms, Memory: 9184 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.cc
+++ 백준/Gold/1753. 최단경로/최단경로.cc
@@ -1,37 +1,35 @@
 #include <bits/stdc++.h>
-
 using namespace std;
-
+#define fastio cin.tie(0), ios_base::sync_with_stdio(0)
+#define pii pair<int,int>
 #define SIZE 20'002
-#define INF 1e9+1
-int V, E, start, d[SIZE];
-vector<pair<int, int>> adj[SIZE]; // 비용, 간선
-priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+
+int v, e, s, d[SIZE];
+vector<pii > adj[SIZE]; // pair<cost, nxt>
+priority_queue<pii, vector<pii >, greater<> > pq; // pair<cost, nxt>
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cin >> V >> E >> start;
-    for (int i = 0; i < E; i++) {
+    fastio;
+    cin >> v >> e >> s;
+    while (e--) {
         int u, v, w;
         cin >> u >> v >> w;
         adj[u].push_back({w, v});
     }
-    fill(d, d + SIZE, INF);
-    d[start] = 0;
-    pq.push({d[start], start});
+    fill(&d[0], &d[SIZE], 1e9);
+    d[s] = 0, pq.push({d[s], s});
     while (!pq.empty()) {
-        auto cur = pq.top();
+        auto now = pq.top();
         pq.pop();
-        if (d[cur.second] != cur.first) continue;
-        for (auto nxt: adj[cur.second]) {
-            if (d[nxt.second] <= d[cur.second] + nxt.first) continue;
-            d[nxt.second] = d[cur.second] + nxt.first;
+        if (d[now.second] != now.first) continue;
+        for (auto nxt: adj[now.second]) {
+            if (d[nxt.second] <= d[now.second] + nxt.first) continue;
+            d[nxt.second] = d[now.second] + nxt.first;
             pq.push({d[nxt.second], nxt.second});
         }
     }
-    for (int i = 1; i <= V; i++) {
-        if (d[i] == INF) cout << "INF\n";
+    for (int i = 1; i <= v; i++) {
+        if (d[i] == 1e9) cout << "INF\n";
         else cout << d[i] << '\n';
     }
     return 0;
