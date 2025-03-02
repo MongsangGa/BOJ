7e1e7d106badc5dac558fd32346a27820a1bab37
MongsangGa <88891106+MongsangGa@users.noreply.github.com>
2025-03-02 15:35:47 +0900

    [level 3] Title: 등굣길, Time: 0.05 ms, Memory: 3.8 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.cpp
+++ 프로그래머스/3/42898. 등굣길/등굣길.cpp
@@ -1,22 +1,18 @@
 #include <bits/stdc++.h>
-#define MOD 1000000007
-
 using namespace std;
+#define MOD 1'000'000'007
 
 int dp[102][102];
 
 int solution(int m, int n, vector<vector<int>> puddles) {
-    int answer = 0;
-
-    for(auto p : puddles) dp[p[0]][p[1]] = -1;
-
-    dp[1][1]=1;
-
-    for(int i=1; i<=m; i++) {
-        for(int j=1; j<=n; j++) {
+    for(auto p:puddles) 
+        dp[p[1]][p[0]] = -1;
+    dp[1][1] = 1;
+    for(int i=1;i<=n;i++) 
+        for(int j=1;j<=m;j++) {
+            if(i == 1 && j == 1) continue;
             if(dp[i][j] == -1) dp[i][j] = 0;
-            else dp[i][j] += (dp[i-1][j] + dp[i][j-1]) % MOD;
+            else if(dp[i][j] != -1) dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % MOD;
         }
-    }
-    return answer=dp[m][n];
+    return dp[n][m];
 }
