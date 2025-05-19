afabac2d36e39498ef44e25a9d58076609d5546f
MongsangGa <88891106+MongsangGa@users.noreply.github.com>
2025-05-19 22:47:48 +0900

    [Silver III] Title: N과 M (1), Time: 24 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -1,28 +1,26 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-int N,M,arr[9],check[9];
-int solve(int x){
-    if(x==M){
-        for(int i=0;i<M;i++)
-            cout<<arr[i]<<' ';
-        cout<<'\n';
-        return 0;
+#define fastio cin.tie(0), ios_base::sync_with_stdio(0)
+
+int n, m, a[10], v[10];
+
+void DFS(int idx) {
+    if (idx == m) {
+        for (int i = 0; i < m; i++) cout << a[i] << ' ';
+        cout << '\n';
+        return;
     }
-    for(int i=1;i<=N;i++){
-        if(!check[i]){
-            check[i]=1;
-            arr[x]=i;
-            solve(x+1);
-            check[i]=0;
-        }
+    for (int i = 1; i <= n; i++) {
+        if (v[i]) continue;
+        a[idx] = i, v[i] = 1;
+        DFS(idx + 1);
+        v[i] = 0;
     }
-    return 0;
 }
-int main(){
-    cin.tie(NULL);
-    cout.tie(NULL);
-    ios_base::sync_with_stdio(false);
-    cin>>N>>M;
-    solve(0);
+
+int main() {
+    fastio;
+    cin >> n >> m;
+    DFS(0);
     return 0;
 }
