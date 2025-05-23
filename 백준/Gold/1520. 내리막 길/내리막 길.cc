68f00fec476b1cbba1758e394617023eb15d94ca
MongsangGa <88891106+MongsangGa@users.noreply.github.com>
2025-05-23 10:19:42 +0900

    [Gold III] Title: 내리막 길, Time: 24 ms, Memory: 4392 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.cc
+++ 백준/Gold/1520. 내리막 길/내리막 길.cc
@@ -1,31 +1,30 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+#define fastio cin.tie(0), ios_base::sync_with_stdio(0)
+#define S 500
 
-#define MAX 501
+int n, m, a[S][S], dp[S][S], dx[] = {-1, 0, 1, 0}, dy[] = {0, -1, 0, 1};
 
-int n, m, arr[MAX][MAX], dp[MAX][MAX], dx[] = {1, 0, -1, 0}, dy[] = {0, 1, 0, -1};
-
-int solve(int x, int y) {
+int DFS(int x, int y) {
+    if (dp[x][y] != -1) return dp[x][y];
     if (x == n - 1 && y == m - 1) return 1;
-    if (dp[x][y] != -1)
-        return dp[x][y];
     dp[x][y] = 0;
     for (int i = 0; i < 4; i++) {
         int nx = x + dx[i], ny = y + dy[i];
-        if (arr[nx][ny] >= arr[x][y] || nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
-        dp[x][y] += solve(nx, ny);
+        if (a[x][y] <= a[nx][ny] || nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
+        dp[x][y] += DFS(nx, ny);
     }
     return dp[x][y];
 }
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
+    fastio;
     cin >> n >> m;
     for (int i = 0; i < n; i++)
         for (int j = 0; j < m; j++)
-            cin >> arr[i][j];
-    memset(dp, -1, sizeof(dp));
-    cout << solve(0, 0);
+            cin >> a[i][j];
+    fill(dp[0], dp[S], -1);
+    cout << DFS(0, 0);
+    return 0;
 }
