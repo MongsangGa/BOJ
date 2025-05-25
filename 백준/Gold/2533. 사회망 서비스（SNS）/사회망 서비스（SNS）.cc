bcc1546c22fa3b0e0d733228bf87af4d836bcfcd
MongsangGa <88891106+MongsangGa@users.noreply.github.com>
2025-05-25 16:51:21 +0900

    [Gold III] Title: 사회망 서비스(SNS), Time: 864 ms, Memory: 73572 KB -BaekjoonHub

--- 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.cc
+++ 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.cc
@@ -1,35 +1,30 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
 #define fastio cin.tie(0), ios_base::sync_with_stdio(0)
-#define SIZE 1'000'002
+#define S 1'000'002
 
-int n, x, y, dp[2][SIZE]; // dp[0][now] : 얼리어답터, dp[1][now] : 일반인
-bool vis[SIZE];
-vector<int> adj[SIZE];
+int n, vis[S], dp[2][S];
+vector<int> adj[S];
 
 void DFS(int now) {
-    vis[now] = 1;
-    dp[0][now] = 1;
+    vis[now] = 1, dp[1][now] = 1;
     for (int nxt: adj[now]) {
         if (vis[nxt]) continue;
         DFS(nxt);
-        dp[0][now] += min(dp[0][nxt], dp[1][nxt]);
-        dp[1][now] += dp[0][nxt];
+        dp[1][now] += min(dp[0][nxt], dp[1][nxt]);
+        dp[0][now] += dp[1][nxt];
     }
 }
 
 int main() {
     fastio;
     cin >> n;
-    for (int i = 0; i < n - 1; i++) {
-        cin >> x >> y;
-        adj[x].push_back(y);
-        adj[y].push_back(x);
+    for (int i = 0, u, v; i < n - 1; i++) {
+        cin >> u >> v;
+        adj[u].push_back(v), adj[v].push_back(u);
     }
     DFS(1);
     cout << min(dp[0][1], dp[1][1]);
     return 0;
 }
-
