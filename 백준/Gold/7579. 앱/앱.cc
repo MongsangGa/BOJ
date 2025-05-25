2dc175de5d2796312919a1d2e5ffe9dbeca09060
MongsangGa <88891106+MongsangGa@users.noreply.github.com>
2025-05-25 16:07:02 +0900

    [Gold III] Title: 앱, Time: 4 ms, Memory: 6544 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.cc
+++ 백준/Gold/7579. 앱/앱.cc
@@ -1,28 +1,24 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
-#define SIZE 101
-
-int n, maxM, maxC, m[SIZE], c[SIZE], dp[SIZE][SIZE * SIZE];
+#define fastio cin.tie(0), ios_base::sync_with_stdio(0)
+#define S 105
+int N, M, C, m[S], c[S], dp[S][S * S];
 
 int main() {
-    cin >> n >> maxM;
-    for (int i = 1; i <= n; i++) cin >> m[i];
-    for (int i = 1; i <= n; i++) {
-        cin >> c[i];
-        maxC += c[i];
-    }
-    for (int i = 1; i <= n; i++) {
-        for (int j = 0; j <= maxC; j++) {
+    fastio;
+    cin >> N >> M;
+    for (int i = 1; i <= N; i++) cin >> m[i];
+    for (int i = 1; i <= N; i++) cin >> c[i], C += c[i];
+    for (int i = 1; i <= N; i++)
+        for (int j = 0; j <= C; j++) {
             if (j - c[i] >= 0) dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - c[i]] + m[i]);
             else dp[i][j] = dp[i - 1][j];
         }
-    }
-    for (int j = 0; j <= maxC; j++) {
-        if (dp[n][j] >= maxM) {
-            cout << j;
+    for (int i = 0; i <= C; i++)
+        if (dp[N][i] >= M) {
+            cout << i;
             return 0;
         }
-    }
     return 0;
 }
